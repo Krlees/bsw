f82a3ab69addd1aed5a66ee932679437fe1e4fdb
Krlee <515961601@qq.com>
2017-06-18 11:45:35 +0800

    准备微信接口

---
+++ app/Http/Controllers/Api/ThirdController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Api\BaseController;
+
+class ThirdController extends BaseController
+{
+    /**
+     * 第三方app微信登录
+     */
+    public function appWxLogin()
+    {
+
+    }
+
+    /**
+     * 第三方qq登录
+     */
+    public function appQqLogin()
+    {
+
+    }
+
+    /**
+     * 第三方微信PC端扫码登录
+     */
+    public function pcWxLogin()
+    {
+
+    }
+
+    /**
+     * 第三方QQ PC端登录
+     */
+    public function pcQqLogin()
+    {
+
+    }
+
+    /**
+     * 第三方手机版微信登录
+     */
+    public function wapWxLogin()
+    {
+
+    }
+
+    /**
+     * 第三方手机版QQ登录
+     */
+    public function wapQqLogin()
+    {
+
+    }
+}
