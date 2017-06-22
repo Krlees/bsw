0cd62f10936386f12ed009c56199bd94bad00053
Krlee <515961601@qq.com>
2017-06-22 14:34:25 +0800

    ws

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -33,7 +33,6 @@ class TransactionController extends BaseController
      */
     public function getVipList(Request $request, Label $label, Transaction $transaction, UserVip $userVip)
     {
-        $city = $request->input('city'); //默认获取全部
         $pages = $this->pageInit();
         $channelId = 1;
 
@@ -93,12 +92,63 @@ class TransactionController extends BaseController
     }
 
     /**
-     * 获取订单资讯信息
+     * 获取订单频道资讯信息
      * @param Request $request
      * @param Transaction $transaction
      */
-    public function getOrderList()
+    public function getOrderList(Request $request, Label $label, Transaction $transaction, UserVip $userVip)
     {
+        $pages = $this->pageInit();
+        $channelId = 2; //订单频道
+
+        $where[] = ['channel_id', '=', $channelId];
+        $return = [];
+        $labelData = $label->getList($label->getTable(), 0, 4);
+        foreach ($labelData as $key => $val) {
+            $where[] = ['label_id', '=', $val['id']];
+            $result = $transaction->getList($pages['page'], 4, $where);
+            if (empty($result)) {
+                continue;
+            }
+
+            // 订单频道逻辑处理
+            $result = $this->_helpOrder($result, $userVip, $transaction);
+
+            $return[$key]['label_id'] = $val['id'];
+            $return[$key]['label_name'] = $val['name'];
+            $return[$key]['list'] = $result;
+
+        }
+    }
+
+    /**
+     * 获取订单频道内列表
+     * @param Request $request
+     * @param Label $label
+     * @param Transaction $transaction
+     * @param UserVip $userVip
+     */
+    public function getOrderInfo(Request $request, Label $label, Transaction $transaction, UserVip $userVip)
+    {
+        $city = $request->input('city'); //默认获取全部
+        $pages = $this->pageInit();
+        $labelId = $request->input('label_id') or $this->responseApi(1004);
+        $channelId = 2;
+
+        $where[] = ['channel_id', '=', $channelId];
+        $where[] = ['label_id', '=', $labelId];
+        if ($city && $city != '全部') {
+            $where[] = ['city', '=', $city];
+        }
+
+        $result = $transaction->getList($pages['page'], $pages['limit'], $where);
+        if (empty($result)) {
+            $this->responseApi(0, '', $result);
+        }
+
+        $result = $this->_helpOrder($result, $userVip, $transaction);
+
+        $this->responseApi(0, '', $result);
 
     }
 
@@ -196,6 +246,61 @@ class TransactionController extends BaseController
         }
 
         return $result;
+    }
+
+    private function _helpOrder($result, $userVip, $transaction)
+    {
+        foreach ($result as $k=>$v) {
+
+            /* 判断当前用户VIP是否有效 */
+            if ($this->user_ses) {
+                if ($userVip->checkExpires($v['label_id'], $this->user_ses->id))
+                    $isVip = true;
+            }
+
+            // 1. 判断是否是自己发的，自己发的自动解锁
+            if ($this->user_ses) {
+                if ($this->user_ses->id == $v['user_id']) {
+                    $lock = false;
+                    $result[$k]['lock'] = $lock;
+                    $result[$k]['isVip'] = $isVip;
+                    continue;
+                }
+            }
+
+            // 2. 检测用户是否购买
+            if ($this->checkUserPay($v['id'], $transaction)) {
+                unset($result[$k]);
+                continue;
+            }
+
+            // 超过72小时，归为0
+            $lockTime = time() - strtotime($v['created_at']); //剩余时间
+            if ($lockTime > 72 * 60 * 60) {
+                $lockTime = 0;
+            }
+
+            // 3. 判断是否需要正常购买，【VIP】有效
+            if ($v['is_normal_pay']) {
+
+                /* 判断VIP */
+                if ($isVip) {
+                    $lock = false;
+                    $result[$k]['lock'] = $lock;
+                    $result[$k]['isVip'] = $isVip;
+                    continue;
+                }
+            }
+
+            // 4. 超过72小时，可免费查看
+            if ($lockTime == 0) {
+                $lock = false;
+            }
+
+            $result[$k]['dueTime'] = $lockTime; //剩余时间
+            $result[$k]['lock'] = $lock;
+            $result[$k]['isVip'] = $isVip;
+        }
     }
 
 }
